4be4f4b8bb68308174ef466ead5095afc61cef8d
Ying Guo <guoguoying2013@gmail.com>
2021-01-23 13:24:29 -0800

    built collections for users and questions, created controller methods

---
+++ db/index.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/gamification', { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Schemas
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  hash: String,
+  salt: String,
+});
+
+const questionsSchema = new mongoose.Schema({
+  id: Number,
+  question: String,
+  status: Number,
+});
+
+// Collections
+const Users = mongoose.model('user', userSchema);
+const Questions = mongoose.model('question', questionsSchema);
+
+// Controller methods
+const createNewUser = (newUserInfo) => {
+  const doc = new Users(newUserInfo);
+  doc.save()
+    .then(() => 'saved!')
+    .catch((err) => err);
+};
+
+const getQuestions = () => {
+  Questions.find({})
+    .then((data) => data);
+    .catch((err) => err);
+};
+
+const getHashSalt = (email) => {
+  Users.find({email: email})
+    .then((res) => res);
+    .catch((err) => err);
+};
+
+module.exports = {
+  createNewUser,
+  getQuestions,
+  getHashSalt,
+}
+

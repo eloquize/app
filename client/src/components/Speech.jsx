d1e3c08bcf7f0f22431210258fcc50615e781ac2
Annie Heo <annie.heo@gmail.com>
2021-01-23 14:08:06 -0800

    implment functions for user to expand voice recording for more than 15 seconds

--- client/src/components/Speech.jsx
+++ client/src/components/Speech.jsx
@@ -6,10 +6,10 @@ const { api, region } = require('../utils/speech.js');
 const speechConfig = sdk.SpeechConfig.fromSubscription(api, region);
 
 export default function Speech() {
-  const [typed, changeTyped] = useState('');
+  const [speech, changeSpeech] = useState('');
 
   const handleChange = (e) => {
-    changeTyped(e.target.value);
+    changeSpeech(e.target.value);
   };
 
   const fromMic = () => {
@@ -17,21 +17,35 @@ export default function Speech() {
     const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
     console.log('Speak into your microphone.');
+
+    //start recognizing audio
     recognizer.recognizeOnceAsync((result) => {
-      changeTyped(result.text);
+      changeSpeech(result.text);
       console.log(`RECOGNIZED: Text=${result.text}`);
     });
   };
 
-  console.log(typed);
+  const stopMic = () => {
+    const recognizer = new sdk.SpeechRecognizer(speechConfig);
+
+    recognizer.sessionStopped = (s, e) => {
+        console.log("\n    Session stopped event.");
+        recognizer.stopContinuousRecognitionAsync();
+    };
+  };
+
+  console.log(speech);
 
   return (
     <div>
-      <button type="button" onClick={() => { fromMic(); }}>mic on</button>
+      <button type="button" onClick={() => { fromMic(); }}>start recording</button>
+      <button type="button" onClick={() => { stopMic(); }}>stop recording</button>
       <form>
-          <label>Diction:
-          <textarea value={typed} onChange={handleChange} />
-          </label>
+        <label>
+          Diction:
+          <div />
+          <textarea value={speech} onChange={handleChange} />
+        </label>
       </form>
     </div>
   );

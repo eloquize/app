78a25dea224ffabae7583f20438f90751162b823
Jordan Weiner <weinerj8@gmail.com>
2021-01-24 11:01:49 -0800

    Begin refactor to styled-components, starting with App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,10 +4,28 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 import { getQuestions } from '../utils/api';
 import Login from './Login';
 import Questions from './Questions';
 import Speech from './Speech';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Comfortaa;
+    font-style: normal;
+    font-weight: normal;
+    color: #000000;
+  }
+`;
+
+const Wrapper = styled.div`
+  background-image: url("../background.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-width: 100vw;
+  min-height: 100vh;
+`;
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -22,8 +40,8 @@ function App() {
 
   return (
     <Router>
-      <div className="wrapper">
-        {/* <img className="background" src="./background.svg" alt="bg" /> */}
+      <GlobalStyle />
+      <Wrapper>
         <Switch>
           <Route path="/questions">
             <Questions questions={questions} />
@@ -35,7 +53,7 @@ function App() {
             <Login />
           </Route>
         </Switch>
-      </div>
+      </Wrapper>
     </Router>
   );
 }

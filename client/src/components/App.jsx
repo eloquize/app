feb639349d83ab69253d696985a51e910d0ca25c
Annie Heo <annie.heo@gmail.com>
2021-01-23 17:28:18 -0800

    basic components set up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getExample } from '../utils/api';
 import Speech from './Speech';
+import Login from './Login';
+import Questions from './Questions';
 
 function App() {
   const [text, setText] = useState('Hello React!');
+  const [login, setLogin] = useState(false);
 
   useEffect(() => {
     // eslint-disable-next-line no-console
@@ -13,6 +16,7 @@ function App() {
   return (
     <div>
       <>{text}</>
+      {login ? <div>hello</div> : <Login /> }
       <Speech />
     </div>
   );
---
+++ client/src/components/Questions.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function Question(){
+ const [questions, setQuestions] = useState([]);
+
+ return (
+     <div>QUESTIONS</div>
+ )
+};
